f1c17de3a93f642f4923aafa20df869c017164f5
Andre-1970 <baikalandrei1970@list.ru>
2024-05-01 16:55:36 +0300

    model

--- src/data_loader.py
+++ src/data_loader.py
@@ -8,20 +8,20 @@ from sklearn.preprocessing import OneHotEncoder
 class DataLoader:
     def __init__(self, base_path, csv_file="train.csv"):
         """
-        Инициализация DataLoader с базовым путем к данным и путем к CSV файлу с метками.
-
+        Инициализирует DataLoader, указывая путь к директории с изображениями и файлом с метками.
         Parameters:
-            base_path (str): Путь к папке с изображениями.
-            csv_file (str): Имя файла CSV, содержащего метки и пути к изображениям.
+            base_path (str): Путь к папке, содержащей изображения.
+            csv_file (str): Имя файла CSV в указанной директории, содержащего метки и пути к изображениям.
         """
         self.base_path = base_path
         self.labels_df = pd.read_csv(os.path.join(base_path, csv_file))
         self.update_image_paths()
+        self.encoder = OneHotEncoder(sparse_output=False)
+        self.prepare_encoder()
 
     def update_image_paths(self):
         """
-        Обновление путей к изображениям в датафрейме.
-        Преобразует относительные пути, начинающиеся с './train/' на raw/train/.
+        Обновляет пути к изображениям в DataFrame, преобразуя относительные пути в абсолютные.
         """
         self.labels_df["image_path"] = self.labels_df["image_path"].apply(
             lambda x: os.path.abspath(
@@ -31,44 +31,54 @@ class DataLoader:
 
     def load_image(self, path, size=(224, 224)):
         """
-        Загрузка изображения, его ресайзинг и нормализация.
-
+        Загружает изображение, изменяет его размер и нормализует значения пикселей.
         Parameters:
-            path (str): Путь к изображению.
-            size (tuple): Желаемый размер изображения.
-
+            path (str): Абсолютный путь к изображению.
+            size (tuple): Желаемый размер изображения после изменения размера (ширина, высота).
         Returns:
-            numpy.ndarray: Нормализованное изображение.
+            numpy.ndarray: Массив изображения с нормализованными значениями пикселей.
         """
         image = Image.open(path)
         image = image.resize(size)
-        image = np.array(image) / 255.0  # Преобразование пикселей к диапазону [0, 1]
+        image = np.array(image) / 255.0
         return image
 
-    def load_dataset(self):
+    def load_dataset(self, batch_size=None):
         """
-        Загрузка всех изображений и преобразование меток в one-hot вектора.
-
+        Загружает изображения и метки, преобразует метки в формат one-hot.
+        Parameters:
+            batch_size (int, optional): Количество образцов, обрабатываемых за один проход во время обучения сети.
         Returns:
-            tuple: массив numpy с изображениями и массив numpy с one-hot encoded метками.
+            Генератор, возвращающий кортежи (numpy.ndarray, numpy.ndarray) - массив изображений и массив one-hot encoded меток.
         """
-        images = np.array(
-            [self.load_image(path) for path in self.labels_df["image_path"]]
-        )
-        emotions = self.convert_to_onehot(self.labels_df["emotion"])
-        return images, emotions
+        if batch_size:
+            for start in range(0, len(self.labels_df), batch_size):
+                end = start + batch_size
+                images_batch = [
+                    self.load_image(x) for x in self.labels_df["image_path"][start:end]
+                ]
+                emotions_batch = self.convert_to_onehot(
+                    self.labels_df["emotion"][start:end]
+                )
+                yield np.array(images_batch), emotions_batch
+        else:
+            images = [self.load_image(x) for x in self.labels_df["image_path"]]
+            emotions = self.convert_to_onehot(self.labels_df["emotion"])
+            return np.array(images), emotions
+
+    def prepare_encoder(self):
+        """Обучение OneHotEncoder на всех уникальных метках в датафрейме."""
+        emotions = self.labels_df["emotion"].unique().reshape(-1, 1)
+        self.encoder.fit(emotions)
 
     def convert_to_onehot(self, emotions):
         """
-        Преобразование меток эмоций в one-hot encoded вектора.
-
+        Преобразует список меток эмоций в формат one-hot encoded.
         Parameters:
-            emotions (pandas.Series): Серия меток эмоций.
-
+            emotions (pandas.Series): Список меток эмоций.
         Returns:
-            numpy.ndarray: Массив one-hot encoded меток.
+            numpy.ndarray: Массив one-hot encoded меток, где каждый ряд соответствует одному вектору меток.
         """
-        encoder = OneHotEncoder(sparse=False)
         emotions = np.array(emotions).reshape(-1, 1)
-        onehot_encoded = encoder.fit_transform(emotions)
+        onehot_encoded = self.encoder.transform(emotions)
         return onehot_encoded
